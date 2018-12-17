5c21504b57b7cb8677c490fe3fad45ea77d1b25b
luhuijuan <hjluchn@sina.com>
2018-12-17 20:29:53 +0800

    init

---
+++ 2-js/test1-js.js
@@ -0,0 +1,21 @@
+var num1 = 1
+var num2 = 2
+var num3 = 3.1415
+var str1 = 'lhj'
+var str2 = 'abc'
+var str3 = '456'
+
+var plus1 = num1 + num2
+console.log('1 + 2 = ' + plus1)
+
+
+var plus2 = str1 + str2
+console.log('"lhj" + "abc" = ' + plus2)
+
+
+var plus3 = num1 + str1
+console.log('1 + "lhj" = ' + plus3)
+
+var plus4 = num3 + str3
+console.log('3.1415 + "456" = ' + plus4)
+
